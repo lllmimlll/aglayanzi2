1001522ab4025e5b81a56fcb57420b35e64da26c
xozatt <cayanjiyan62@hotmail.com>
2021-01-25 13:31:24 +0300

    now slenderman chasing

--- HorrorMulti/Assets/Scripts/SlenderManChasing.cs
+++ HorrorMulti/Assets/Scripts/SlenderManChasing.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SlenderManChasing : MonoBehaviour
 {
+    public Transform character;
+    private Rigidbody rb;
+    public float speed = 4.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 position = Vector3.MoveTowards(transform.position, character.position, speed * Time.fixedDeltaTime);
+        rb.MovePosition(position);
+        transform.LookAt(character);
     }
 }
