154761a09e96cd294f9bcfc2aae24808dc1401b7
xozatt <cayanjiyan62@hotmail.com>
2021-01-26 16:51:52 +0300

    al

--- HorrorMulti/Assets/Scripts/Mediaval.cs
+++ HorrorMulti/Assets/Scripts/Mediaval.cs
@@ -8,13 +8,12 @@ public class Mediaval : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(character);
-        transform.rotation.Set(0,0,0,0);
     }
 }
