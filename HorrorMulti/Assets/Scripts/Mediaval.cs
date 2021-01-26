107ed813274bec44f804d340106e7051f9cdb737
lllmimlll <yasin27012@gmail.com>
2021-01-26 16:06:01 +0300

    insallahmasallah

---
+++ HorrorMulti/Assets/Scripts/Mediaval.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mediaval : MonoBehaviour
+{
+    public Transform character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(character);
+        transform.rotation.Set(0,0,0,0);
+    }
+}
